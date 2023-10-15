1b6ed4408bf1ad17768650a08071cc3a813f835a
John Connolly <jcconnol4@gmail.com>
2023-10-15 16:22:12 -0500

    add mathing

--- README.md
+++ README.md
@@ -2,7 +2,15 @@
 
 ## To Run
 
+### Comiling and Running jar
+
 run the following command:
 ```bash
 mvn clean install && java -jar .\target\demo-0.0.1-SNAPSHOT.jar
+```
+
+### Running Spring Boot for Development
+
+```bash
+mvn clean install && mvn spring-boot:run
 ```
--- src/main/kotlin/com/example/demo/HelloController.kt
+++ src/main/kotlin/com/example/demo/HelloController.kt
@@ -1,6 +1,8 @@
 package com.example.demo.helloworld
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -8,8 +10,42 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api")
 class HelloController {
 
+    data class Body(val number1: Int, val number2: Int, val operation: String)
+
+    data class Result(val answer: String)
+
     @GetMapping("/hello")
     fun sayHello(): String {
-        return "Hello, World again!"
+        return "Hello, World even more!"
+    }
+
+    @PostMapping("/math")
+    fun addNumbers(@RequestBody body: Body): Result {
+        val number1 = body.number1
+        val number2 = body.number2
+        var answer = "0.0"
+
+        if (body.operation == "+") {
+            answer = (number1 + number2).toString()
+        } else if (body.operation == "-") {
+            answer = (number1 - number2).toString()
+        } else if (body.operation == "/") {
+            if (number2 != 0) {
+                answer = (number1 / number2).toString()
+            } else {
+                return Result("Division by zero")
+            }
+        } else if (body.operation == "*") {
+            answer = (number1 * number2).toString()
+        } else {
+            return Result("Unknown operation")
+        }
+
+        return Result(answer)
+    }
+
+    @GetMapping("/database-select")
+    fun databaseSelect(): String {
+        return "Hello, World!"
     }
 }
